63642ad32b48a1fd005230b1afacf187ae375ac5
Viachaslau <127544303+sv1atsk1@users.noreply.github.com>
2024-05-26 00:16:24 +0300

    Add files via upload

---
+++ Two Sum/src/main/java/by/viachaslau/Solution.java
@@ -0,0 +1,32 @@
+package by.viachaslau;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+
+}
