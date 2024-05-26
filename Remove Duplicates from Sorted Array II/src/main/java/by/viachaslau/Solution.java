6b7056f7de258949313875925af9fc2b2d2a7be7
Viachaslau <127544303+sv1atsk1@users.noreply.github.com>
2024-05-26 20:41:54 +0300

    Update Solution.java

--- Remove Duplicates from Sorted Array II/src/main/java/by/viachaslau/Solution.java
+++ Remove Duplicates from Sorted Array II/src/main/java/by/viachaslau/Solution.java
@@ -5,8 +5,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
-        int[] nums = {1,1,1,2,2,3};
+        int[] nums = {0, 1};
         int k = solution.removeDuplicates(nums);
 
         System.out.println("Number of unique elements: " + k);
@@ -18,18 +17,17 @@ public class Solution {
 
     public int removeDuplicates(int[] nums) {
 
-        if (nums.length == 0) {
-            return 0;
-        }
+        if (nums.length == 0) return 0;
+
+        if (nums.length <= 2) return nums.length;
 
         int k = 2;
 
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i] != nums[k - 2]) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
                 nums[k] = nums[i];
                 k++;
             }
-
         }
         return k;
     }
