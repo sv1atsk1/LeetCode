754fe6770d7b43b1c90925f06962d89e23e26a2a
Viachaslau <127544303+sv1atsk1@users.noreply.github.com>
2024-05-28 18:42:28 +0300

    Add files via upload

---
+++ Jump Game/src/main/java/by/viachaslau/Solution.java
@@ -0,0 +1,15 @@
+package by.viachaslau;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 2, 1, 0, 4};
+        System.out.println(solution.canJump(nums));
+
+
+    }
+
+    public boolean canJump(int[] nums) {
+
+    }
+}
